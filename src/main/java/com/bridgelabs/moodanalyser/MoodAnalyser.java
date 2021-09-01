3fd7dbdadc2347df3421e54973d8b9a1fd5b8e25
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-09-02 00:25:02 +0530

    [Pritesh] Made constructor with parameters and call method

--- src/main/java/com/bridgelabs/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabs/moodanalyser/MoodAnalyser.java
@@ -1,15 +1,33 @@
 package com.bridgelabs.moodanalyser;
 
 public class MoodAnalyser {
+	String message;
+
+	public MoodAnalyser() {
+
+	}
+
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
+		if (message.contains("Happy"))
+			return "Happy";
+		if (message.toLowerCase().contains("sad"))
+			return "Sad";
+		else
+			return "Happy";
+
+	}
 
 	public String analyseMood(String mood) {
 		if (mood.contains("Happy"))
 			return "Happy";
 		if (mood.toLowerCase().contains("sad"))
 			return "Sad";
-		else 
+		else
 			return "Happy";
-		
 
 	}
 }
